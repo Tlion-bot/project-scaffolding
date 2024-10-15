be1a8c28961690f091f163cdab83940c9aa829ec
liuyinghua <1982570424@qq.com>
2024-10-15 16:52:54 +0800

    day0

--- java/Test.java
+++ java/Test.java
@@ -1,4 +1,4 @@
-package com.base.test.java;
+
 
 import java.io.IOException;
 
--- java/calculate/StrHelper.java
+++
@@ -1,7 +0,0 @@
-package com.base.test.java.calculate;
-
-public interface StrHelper {
-    public static String getObjectValue(Object value) {
-        return value != null ? value.toString() : "";
-    }
-}
